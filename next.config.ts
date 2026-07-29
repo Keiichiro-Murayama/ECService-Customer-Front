aecddd4e861f55783aa0fee00a52f2fdcf8becd9
Keiichiro-Murayama <Keiichiro.Murayama@wemex.com>
2026-07-29 14:47:39 +0900

    テスト：認証確認追加

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,44 @@
+// import type { NextConfig } from "next";
+// /**
+//  * APIプロキシ(BFF)の設定を追加する
+//  * Next.js プロジェクトの設定ファイル
+//  * ブラウザのセキュリティ制限（CORS）を回避し、フロントエンドから
+//  * 安全にバックエンドAPIを呼び出すための中継ルールを定義
+//  */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   async rewrites() {
+//     return [
+//       {
+//         /**
+//          * 顧客API用のプロキシ設定
+//          * source: フロントエンド側で呼び出すURL（相対パス）
+//          * destination: 実際にデータを取得しに行くバックエンドURL
+//          * ※画面（/api/users/register）とのURL衝突を避けるため、
+//          *   API専用の入り口として「/proxy-api/」を冠しています。
+//          */
+//         source: "/proxy-api/:path*",
+//         destination: "http://20.78.59.178:8080/api/customer/:path*", //Azure用
+//         // destination: "http://localhost:5221/api/customer/:path*", //ローカル用
+//       },
+//       {
+//         /**
+//          * 管理者API用のプロキシ設定
+//          * source: フロントエンド側で呼び出すURL（相対パス）
+//          * destination: 実際にデータを取得しに行くバックエンドURL
+//          * ※画面（/api/users/register）とのURL衝突を避けるため、
+//          *   API専用の入り口として「/proxy-api/」を冠しています。
+//          */
+//         source: "/proxy-api/admin/:path*",
+//         destination: "http://20.78.59.178/api/admin/:path*",
+//       },
+//     ];
+//   },
+// };
+// export default nextConfig;
+
 import type { NextConfig } from "next";
-/**
- * APIプロキシ(BFF)の設定を追加する
- * Next.js プロジェクトの設定ファイル
- * ブラウザのセキュリティ制限（CORS）を回避し、フロントエンドから
- * 安全にバックエンドAPIを呼び出すための中継ルールを定義
- */
-const nextConfig: NextConfig = {
-  /* config options here */
-  async rewrites() {
-    return [
-      {
-        /**
-         * 顧客API用のプロキシ設定
-         * source: フロントエンド側で呼び出すURL（相対パス）
-         * destination: 実際にデータを取得しに行くバックエンドURL
-         * ※画面（/api/users/register）とのURL衝突を避けるため、
-         *   API専用の入り口として「/proxy-api/」を冠しています。
-         */
-        source: "/proxy-api/:path*",
-        destination: "http://20.78.59.178:8080/api/customer/:path*", //Azure用
-        // destination: "http://localhost:5221/api/customer/:path*", //ローカル用
-      },
-      {
-        /**
-         * 管理者API用のプロキシ設定
-         * source: フロントエンド側で呼び出すURL（相対パス）
-         * destination: 実際にデータを取得しに行くバックエンドURL
-         * ※画面（/api/users/register）とのURL衝突を避けるため、
-         *   API専用の入り口として「/proxy-api/」を冠しています。
-         */
-        source: "/proxy-api/admin/:path*",
-        destination: "http://20.78.59.178/api/admin/:path*",
-      },
-    ];
-  },
-};
+
+const nextConfig: NextConfig = {};
+
 export default nextConfig;
